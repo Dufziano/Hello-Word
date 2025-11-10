2e75f168e51cb6f25af4d7b9b1fa3792d10292ee
Dufziano <A01803792@tec.mx>
2025-11-10 13:53:55 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string minombre;
+    cout << "Escribe tu nombre: ";
+    cin >> minombre;
+    cout << "Hola " << minombre << " !" << endl;
+}
